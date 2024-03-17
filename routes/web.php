e39baae8ae70e9f6a9cbadfaac58be99ba2ec1ae
Abu Bakar Pervaiz <56159572+Abu-bakar-pervaiz@users.noreply.github.com>
2024-03-17 12:36:14 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KeyController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('login',[AuthController::class,'index'])->name('login');
 Route::post('login',[AuthController::class,'login'])->name('login.check');
 Route::get('logout',[AuthController::class,'logout'])->name('logout');
@@ -28,3 +17,8 @@ Route::group(['prefix'=>'key','middleware'=>'auth'],function(){
 
 Route::view('key','guest.check-key')->name('key.check');
 Route::post('key/check',[KeyController::class,'check'])->name('key.check');
+
+// Redirect the Remaining Routes to default route
+Route::fallback(function() {
+    return redirect('key');
+});
